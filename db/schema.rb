206847ad6af942049b9386fe8aceb430e494cd87
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-08-05 12:32:10 -0400

    update workerholic gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170728023625) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
   end
