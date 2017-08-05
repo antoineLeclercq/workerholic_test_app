fd7450e1f19320df6f07dbc263ea9e104ba79ad0
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-08-04 20:50:49 -0400

    map root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'workerholic/web/application'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'test_jobs#index'
   resources :test_jobs, only: [:index]
 
   mount WorkerholicWeb => '/workerholic'
